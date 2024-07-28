ddd19ff970678e6385444b85abd1defaeb20572d
Cody Tolene <codyatolene@gmail.com>
2024-07-28 17:40:05 -0500

    Improve plasma view, eliminate hang with async ops.

--- scripts/views/plasma_view.py
+++ scripts/views/plasma_view.py
@@ -9,26 +9,23 @@ from picographics import PicoGraphics, DISPLAY_GALACTIC_UNICORN as DISPLAY
 
 
 async def run(galacticUnicorn, graphics):
-    width = galacticUnicorn.WIDTH  # Get the width of the display
-    height = galacticUnicorn.HEIGHT  # Get the height of the display
+    width = galacticUnicorn.WIDTH
+    height = galacticUnicorn.HEIGHT
 
     while True:
-        t = time.ticks_ms() / 1000  # Get the current time in seconds
+        t = time.ticks_ms() / 1000  # Current time in seconds
         for y in range(height):
+            await uasyncio.sleep_ms(0)
             for x in range(width):
-                # Calculate the color based on sine and cosine functions
                 sine_component = math.sin(x / width * 8 + t)
                 cosine_component = math.cos(y / height * 8 + t)
                 color_value = (sine_component + cosine_component) * 127.5
                 color = int(color_value + 127.5)
 
-                # Set the pen to the calculated color
                 graphics.set_pen(graphics.create_pen(color, 0, 255 - color))
-                # Draw the pixel at (x, y) with the current pen color
                 graphics.pixel(x, y)
-        # Update the display with the new graphics
+
         galacticUnicorn.update(graphics)
-        # Wait for a short time before the next update
         await uasyncio.sleep(0.1)
 
 
